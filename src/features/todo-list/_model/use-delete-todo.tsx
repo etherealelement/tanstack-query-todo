60dd545fa1d5c6ff54b7be535ec0f313901ad672
etherealelement <vitalik.safonov@list.ru>
2024-10-19 09:08:12 +0300

    add: delete todo update

--- src/features/todo-list/_model/use-delete-todo.tsx
+++ src/features/todo-list/_model/use-delete-todo.tsx
@@ -6,10 +6,19 @@ export function useDeleteTodo() {
 
   const deleteTodoMutation = useMutation({
     mutationFn: todoListApi.deleteTodo,
-    onSettled: async () => {
-      return await queryClient.invalidateQueries(
-        todoListApi.getTodoListQueryOptions()
+    async onSettled() {
+      queryClient.invalidateQueries(todoListApi.getTodoListQueryOptions());
+    },
+    async onSuccess(_, deletedId) {
+      const todos = queryClient.getQueryData(
+        todoListApi.getTodoListQueryOptions().queryKey
       );
+      if (todos) {
+        queryClient.setQueryData(
+          todoListApi.getTodoListQueryOptions().queryKey,
+          todos.filter(todo => todo.id !== deletedId)
+        );
+      }
     }
   });
 
